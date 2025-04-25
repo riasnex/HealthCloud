48720b560283a6fee7b343f74a56c309925be202
Dani-EL096 <164419791+Dani-EL096@users.noreply.github.com>
2025-04-25 03:28:37 -0400

    Avance en Login

--- hc-front/src/App.jsx
+++ hc-front/src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { PagLogin } from './paginas/paglogin'
 
 
 function App() {
-  return <div>APP</div>
+  return <div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PagLogin/>} />
+      </Routes>
+    </BrowserRouter>
+  </div>
 }
 
 export default App
--- hc-front/src/paginas/paglogin.jsx
+++ hc-front/src/paginas/paglogin.jsx
@@ -1,20 +1,29 @@
 import { useForm } from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
+import { loginSchema } from "../schema/auth.js";
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 export function PagLogin() { 
     const { register, handleSubmit, formState } = useForm({
-        resolver: zodResolver(loginSchema)
+        resolver: zodResolver(loginSchema),
+        
     });
     const onSubmit = handleSubmit((data) => {
         signin(data);
      });
     return (
-        <div>
-            <div className="container bg-sky-400 max-w-md p-6 rouded-md mx-auto mt-10">
+        <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-400 to-blue-600">
+            <div className="container bg-sky-400 max-w-md p-6 rounded-lg shadow-lg mx-auto mt-10">
                 <form onSubmit={onSubmit} >
-            
+
+                    <input type="text" {...register("rut", { required: true})} className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2' placeholder="Rut" />
+                    <p classname="text-red-500">{formState.errors.rut?.message}</p>
+
+                    <input type="password" {...register("password", { required: true})} className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2' placeholder="Contraseña" />
+                    <p classname="text-red-500">{formState.errors.password?.message}</p>
+
+                    <button className='text-white bg-red-600 w-full px-4 py-2 rounded-md'>Iniciar Sesión</button>
                 </form>
             </div>
         </div>
