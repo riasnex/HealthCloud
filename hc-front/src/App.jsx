c985541b8931e7b5a3b34311c53c574db58ebbbd
elmonokl <l.guevaragodoy@gmail.com>
2025-05-22 19:47:46 -0400

    de 0

--- hc-front/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react'
-
-
-function App() {
-  return <div>APP</div>
-}
-
-export default App
--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import PerfilPage from "./pages/PerfilPage";
-
-function App() {
-  return <PerfilPage />;
-}
-
-export default App;
